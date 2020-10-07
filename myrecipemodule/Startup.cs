4faeabc846f60871f78c4545a340c6202ef214ae
Sijmen Koffeman <koffemans@hotmail.com>
2020-10-07 16:57:52 +0100

    Add project files.

---
+++ myrecipemodule/Startup.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace myrecipemodule
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaControllerRoute(
+                name: "Home",
+                areaName: "myrecipemodule",
+                pattern: "Home/Index",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+        }
+    }
+}
